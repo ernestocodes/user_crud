b0a230dbad9adfb66413bd1b7844fd772c9ea5e6
ErnestoCarballo <ernestowcarballo12@gmail.com>
2022-03-22 08:52:12 -0700

    initial commit

---
+++ server.py
@@ -0,0 +1,51 @@
+from flask import Flask, redirect, render_template, request
+from users import User
+app = Flask(__name__)
+
+@app.route('/users')
+def users():
+    users = User.get_all()
+    return render_template('users.html', users=users)
+
+@app.route('/users/new')
+def new_user_form():
+    return render_template('new_user.html')
+
+@app.route('/user/create', methods=['POST'])
+def create_user():
+    print(request.form)
+    User.create(request.form)
+    return redirect('/users')
+
+@app.route('/users/<int:id>')
+def show(id):
+    data = {
+        'id':id
+    }
+    user = User.get_one(data)
+    return render_template('show.html', user=user)
+
+@app.route('/users/<int:id>/edit')
+def edit(id):
+    data = {
+        'id':id
+    }
+    user = User.get_one(data)
+    return render_template('update.html', user=user)
+
+@app.route('/users/update', methods=['POST'])
+def update():
+    print(request.form)
+    User.update_one(request.form)
+    return redirect(f"/users/{request.form['id']}")
+
+@app.route('/users/<int:id>/destroy')
+def delete(id):
+    data = {
+        'id':id
+    }
+    User.delete_one(data)
+    return redirect('/users')
+
+if __name__== '__main__':
+    app.run(debug=True)
